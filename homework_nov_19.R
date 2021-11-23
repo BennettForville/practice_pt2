25090ca7f7fe2815c982ec7a8b0c0df6e72742c3
BennettForville <92814830+BennettForville@users.noreply.github.com>
2021-11-23 14:10:50 -0500

    Update homework_nov_19.R
    added info for variable north_america including summary and plot of lifeExp over time

--- homework_nov_19.R
+++ homework_nov_19.R
@@ -1,8 +1,23 @@
 library(gapminder)
-library(ggplot2)
 list(gapminder)
 summary(gapminder)
+
+#mean life exp
 summary(gapminder$lifeExp)
+
+#subset mexico & mexico avgs
 mexico <- subset(gapminder, country=="Mexico")
 list(mexico)
 summary(mexico$lifeExp)
+
+# plotting mexico pop 
+library(ggplot2)
+ggplot(data=mexico, aes(x=year, y=pop, size=pop))+ geom_point()
+
+# subset canada, US, and mexico
+north_america <- subset(gapminder, country=="Canada"|country=="United States"|country=="Mexico")
+list(north_america)
+summary(north_america$lifeExp)
+
+library(ggplot2)
+ggplot(data=north_america, aes(x=year, y=lifeExp, color=country))+geom_point()+geom_line()+ggtitle("North America lifeExp")
