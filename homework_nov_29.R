e758126864e68fd67e3d4da86e46d316439c8025
BennettForville <92814830+BennettForville@users.noreply.github.com>
2021-12-03 13:19:53 -0500

    Update homework_nov_29.R
    updated comments and spacing

--- homework_nov_29.R
+++ homework_nov_29.R
@@ -3,9 +3,16 @@ library(gapminder)
 library(dplyr)
 summary(gapminder)
 print(gapminder)
+
+# mean gdpPercap, France, 1980s
 gapminder %>%  filter(country=="France" , year %in% c(1982,1987)) %>% summarise(mean(gdpPercap)) 
+
+# Mean lifeExp for each European country
 gapminder %>% filter(continent == "Europe") %>% group_by(country) %>% summarise(mean(lifeExp))
-gapminder %>% filter(continent=="Europe") %>% group_by(year) %>% summarise(weighted.mean(lifeExp)) %>% ggplot(aes(x=year,y=lifeExp,size=lifeExp))+geom_point()+ggtitle("Mean lifeExp for Europe")
-gapminder %>% count(country)                                                                     
+
+# Graphing mean lifeExp of Europe over time
 gapminder %>% filter(continent=="Europe") %>% group_by(year) %>% summarise(mean_lifeExp=weighted.mean(lifeExp,pop))%>% ggplot(aes(x=year,y=mean_lifeExp,size=mean_lifeExp))+geom_point()+ggtitle("Mean lifeExp for Europe")
+
+# How many countries in data set
+gapminder %>% count(country)                                                                     
 length(unique(gapminder$country))
