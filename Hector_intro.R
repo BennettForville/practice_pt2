0d029093f76a0f2a13e6c58671ac058e83830790
BennettForville <92814830+BennettForville@users.noreply.github.com>
2022-02-21 11:41:04 -0500

    carbon tracking vignette

--- Hector_intro.R
+++ Hector_intro.R
@@ -42,3 +42,31 @@ ggplot(compare_results) +
   facet_wrap(~variable, scales = "free_y") +
   guides(color = guide_legend(title = expression(beta)))
 shutdown(core)
+
+# sensativity analysis hector 
+
+run_with_param <- function(core, parameter, value) {
+  old_value <- fetchvars(core, NA, parameter)
+  unit <- as.character(old_value[["units"]])
+  setvar(core, NA, parameter, value, unit)
+  reset(core)
+  run(core)
+  result <- fetchvars(core, 2000:2300)
+  result[["parameter_value"]] <- value
+  result
+}
+
+#' Run Hector with a range of parameter values
+run_with_param_range <- function(core, parameter, values) {
+  mapped <- Map(function(x) run_with_param(core, parameter, x), values)
+  Reduce(rbind, mapped)
+}
+
+sensitivity_beta <- run_with_param_range(core, BETA(), seq(0, 1, 0.05))
+
+ggplot(sensitivity_beta) +
+  aes(x = year, y = value, color = parameter_value, group = parameter_value) +
+  geom_line() +
+  facet_wrap(~variable, scales = "free_y") +
+  guides(color = guide_colorbar(title = expression(beta))) +
+  scale_color_viridis_c() 
